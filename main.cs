cb2439da8209ac164afb7ee96bbf844055ac975f
Lucas-Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-03-11 18:19:40 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+// Created by: Mr Coxall
+// Created on: Sep 2020
+//
+// This program does basic math
+
+using System; 
+
+public class Program 
+{
+
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine("The dimensions of a rectangle are: 5cm and 3cm");
+        Console.WriteLine("The area is " + (5 * 3));
+        Console.WriteLine("The perimeter is " + (2 * (5 + 3)));
+
+        Console.WriteLine("\nDone.");
+    }
+}
